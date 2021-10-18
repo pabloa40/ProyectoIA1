ac452bc8353f61f0198c88d679f9505e6edbb6e4
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-10-18 09:54:24 +0200

    añadidas funciones asing y add en matrix

--- IA1/matrix.cpp
+++ IA1/matrix.cpp
@@ -8,7 +8,7 @@ using namespace std;
 Matrix::Matrix(int rows, int cols) : _rows(rows), _cols(cols)
 {
     //values = new double[_rows][_cols];
-    int totalvalues = _rows * _cols;
+
     values = (double**) malloc(sizeof(double)*_rows);
 
     for(int r=0; r< _rows; r++)
@@ -25,6 +25,11 @@ Matrix::Matrix(int rows, int cols) : _rows(rows), _cols(cols)
     }
 }
 
+Matrix::Matrix(double** v, int rows, int cols): _rows(rows), _cols(cols)
+{
+    values = v;
+}
+
 int Matrix::getCols()
 {
     return _rows;
@@ -38,4 +43,31 @@ int Matrix::getRows()
 double Matrix::get(int row, int col)
 {
     return values[row][col];
+}
+
+void Matrix::asign(int row, int col, double value)
+{
+    values[row][col] = value;
+}
+
+Matrix Matrix::add(Matrix m)
+{
+    Matrix to_return(_rows,_cols);
+
+    if((m.getCols()!=_cols) ||(m.getRows()!=_rows))
+    {
+        throw std::invalid_argument("This Matrix can't be added");
+    }
+    else{
+        for(int i=0;i<_rows; i++)
+        {
+            for(int j=0; j< _cols; j++)
+            {
+
+                to_return.asign(i,j,values[i][j]+ m.get(i,j));
+            }
+        }
+    }
+
+    return to_return;
 }
--- IA1/matrix.h
+++ IA1/matrix.h
@@ -10,17 +10,18 @@ private:
     double **values;
 public:
     Matrix(int rows, int cols);
+    Matrix(double** v,int rows, int cols);
     //~Matrix();
     int getRows();
     int getCols();
     double get(int i, int j);
     Matrix product(Matrix m);
-    Matrix product(double** v, int i, int j);
+    Matrix product(double** v, int rows, int cols);
     Matrix product(int** v, int i, int j);
     Matrix add(Matrix m);
     Matrix add(double** v, int i, int j);
     Matrix add(int** v , int i, int j);
-    Matrix asign(int i, int j, double value);
+    void asign(int row, int col, double value);
 };
 
 
