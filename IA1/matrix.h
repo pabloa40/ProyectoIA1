15a9ddf30682919fdf6472c2f2bbbf9d18402d69
Pablo Rubio Amondarain <pablorubioamodnarain@gmail.com>
2021-10-19 09:37:59 +0200

    comentados métodos de matrix.h sin utilizar o implementar.

--- IA1/matrix.h
+++ IA1/matrix.h
@@ -18,11 +18,11 @@ public:
     int getCols();
     double get(int row, int col);
     Matrix product(Matrix m);
-    Matrix product(double** v, int rows, int cols);
-    Matrix product(int** v, int i, int j);
+    //Matrix product(double** v, int rows, int cols);
+    //Matrix product(int** v, int i, int j);
     Matrix add(Matrix m);
-    Matrix add(double** v, int i, int j);
-    Matrix add(int** v , int i, int j);
+    //Matrix add(double** v, int i, int j);
+    //Matrix add(int** v , int i, int j);
     void asign(int row, int col, double value);
     string toString();
 };
